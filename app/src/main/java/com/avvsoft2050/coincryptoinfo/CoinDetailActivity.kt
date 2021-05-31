bc793cf37a05a576ba9c51211270066fa6743317
Anton2050 <vasilev.anton.v@gmail.com>
2021-05-31 17:03:30 +0300

    fix bug with tvMinPriceLabel layout

--- app/src/main/java/com/avvsoft2050/coincryptoinfo/CoinDetailActivity.kt
+++ app/src/main/java/com/avvsoft2050/coincryptoinfo/CoinDetailActivity.kt
@@ -8,10 +8,14 @@ import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.avvsoft2050.coincryptoinfo.ui.main.CoinsMarketsViewModel
+import kotlinx.android.synthetic.main.activity_coin_detail.*
+import kotlin.math.roundToInt
 
 class CoinDetailActivity : AppCompatActivity() {
 
     private lateinit var coinsMarketsViewModel: CoinsMarketsViewModel
+    var currencyLabel = "$"
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +29,39 @@ class CoinDetailActivity : AppCompatActivity() {
         symbol?.let {
             coinsMarketsViewModel.getCoinInfo(it).observe(this, Observer {
                 Log.d("DETAIL_INFO", it.toString())
+                val red = resources.getColor(android.R.color.holo_red_light)
+                val green = resources.getColor(android.R.color.holo_green_light)
+                tvFirstCurrentPriceD.text = it.currentPrice.toString()
+                tvMinPrice.text = "$currencyLabel${it.low24h}"
+                tvMaxPrice.text = "$currencyLabel${it.high24h}"
+                it.priceChangePercentage1hInCurrency?.let {
+                    val changeHour = (it * 100).roundToInt() / 100.0
+                    if (it < 0) {
+                        tvPercentage1Hour.setTextColor(red)
+                    } else {
+                        tvPercentage1Hour.setTextColor(green)
+                    }
+                    tvPercentage1Hour.text = changeHour.toString()
+                }
+                it.priceChangePercentage24hInCurrency?.let {
+                    val change24Hours = (it * 100).roundToInt() / 100.0
+                    if (it < 0) {
+                        tvPercentage24Hour.setTextColor(red)
+                    } else {
+                        tvPercentage24Hour.setTextColor(green)
+                        tvPercentage24Hour.text = change24Hours.unaryPlus().toString()
+                    }
+                    tvPercentage24Hour.text = change24Hours.toString()
+                }
+                it.priceChangePercentage7dInCurrency?.let {
+                    val change7Days = (it * 100).roundToInt() / 100.0
+                    if (it < 0) {
+                        tvPercentage7Days.setTextColor(red)
+                    } else {
+                        tvPercentage7Days.setTextColor(green)
+                    }
+                    tvPercentage7Days.text = change7Days.toString()
+                }
             })
         }
     }
@@ -32,7 +69,7 @@ class CoinDetailActivity : AppCompatActivity() {
     companion object {
         private const val EXTRA_SYMBOL = "symbol"
 
-        fun newIntent(context: FragmentActivity?, symbol: String): Intent{
+        fun newIntent(context: FragmentActivity?, symbol: String): Intent {
             val intent = Intent(context, CoinDetailActivity::class.java)
             intent.putExtra(EXTRA_SYMBOL, symbol)
             return intent
